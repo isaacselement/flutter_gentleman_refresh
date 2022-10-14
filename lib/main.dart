be1efe28140151779bfe3cb008eed7f43e79bc32
jun.shen <shenj6@xiaopeng.com>
2022-10-14 20:33:52 +0800

    add run zone

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gentleman_refresh/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runZonedGuarded(() {
+    FlutterExceptionHandler? originalOnError = FlutterError.onError;
+    FlutterError.onError = (FlutterErrorDetails details) {
+      originalOnError?.call(details);
+      print('@@@ON_ERROR_ex: ${details.exception}');
+      print('@@@ON_ERROR_st: ${details.stack}');
+    };
+    runApp(const MyApp());
+  }, (error, stack) {
+    print('@@@UNCAUGHT_ERROR: $error');
+    print('@@@UNCAUGHT_STACK: $stack');
+  });
+
 }
 
 class MyApp extends StatelessWidget {
